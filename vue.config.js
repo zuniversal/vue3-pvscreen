cd662cff6a75c44366f8f985032fb6867dbfcb1f
zuniversal <604688489@qq.com>
2023-04-22 17:52:04 +0800

    edit: 修改脚本启动包

--- vue.config.js
+++ vue.config.js
@@ -1,69 +1,66 @@
-const path = require('path')
+const path = require('path');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 function resolve(dir) {
-  return path.join(__dirname, dir)
+	return path.join(__dirname, dir);
 }
 module.exports = {
-  publicPath: './',
-  transpileDependencies: ['webpack-dev-server/client'],
-  chainWebpack: config => {
-   config.entry.app = ['babel-polyfill', './src/main.js'];
-  },
-  chainWebpack(config) {
-    config.module
-      .rule('svg')
-      .exclude.add(resolve('src/icons'))
-      .end()
-    config.module
-      .rule('icons')
-      .test(/\.svg$/)
-      .include.add(resolve('src/icons'))
-      .end()
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'
-      })
-      .end()
-  },
-  devServer: {
-    proxy: {
-      '/api': {
-        target: 'http://106.15.92.117:9510',
-        changeOrigin: true,
-        pathRewrite: {
-          '^/api': '',
-        },
-      },
-    },
-  },
-  css: {
-    loaderOptions: {
-      less: {
-        lessOptions: {
-          modifyVars: {
-          },
-          javascriptEnabled: true,
-        },
-      },
-    },
-  },
-  // configureWebpack: {
-  //   // 代码压缩
-  //   plugins: [
-  //     new UglifyJsPlugin({
-  //       uglifyOptions: {
-  //         //生产环境自动删除console
-  //         compress: {
-  //           drop_debugger: true,
-  //           drop_console: true,
-  //           pure_funcs: ['console.log']
-  //         }
-  //       },
-  //       sourceMap: false,
-  //       parallel: true
-  //     })
-  //   ]
-  // },
-}
+	publicPath: './',
+	transpileDependencies: ['webpack-dev-server/client'],
+	chainWebpack: (config) => {
+		//  config.entry.app = ['babel-polyfill', './src/main.js'];
+		config.entry.app = ['core-js', './src/main.js'];
+	},
+	chainWebpack(config) {
+		config.module.rule('svg').exclude.add(resolve('src/icons')).end();
+		config.module
+			.rule('icons')
+			.test(/\.svg$/)
+			.include.add(resolve('src/icons'))
+			.end()
+			.use('svg-sprite-loader')
+			.loader('svg-sprite-loader')
+			.options({
+				symbolId: 'icon-[name]',
+			})
+			.end();
+	},
+	devServer: {
+		proxy: {
+			'/api': {
+				target: 'http://106.15.92.117:9510',
+				changeOrigin: true,
+				pathRewrite: {
+					'^/api': '',
+				},
+			},
+		},
+	},
+	css: {
+		loaderOptions: {
+			less: {
+				lessOptions: {
+					modifyVars: {},
+					javascriptEnabled: true,
+				},
+			},
+		},
+	},
+	// configureWebpack: {
+	//   // 代码压缩
+	//   plugins: [
+	//     new UglifyJsPlugin({
+	//       uglifyOptions: {
+	//         //生产环境自动删除console
+	//         compress: {
+	//           drop_debugger: true,
+	//           drop_console: true,
+	//           pure_funcs: ['console.log']
+	//         }
+	//       },
+	//       sourceMap: false,
+	//       parallel: true
+	//     })
+	//   ]
+	// },
+};
